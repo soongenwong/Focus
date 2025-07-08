8c420f3a1897d74169ba13addaacc242a918c195
Soong En Wong <wsoongen2015@gmail.com>
2025-07-08 23:15:22 +0100

    Initial Commit

---
+++ Focus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focus
+//
+//  Created by Soong En Wong on 08/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Soong En Wong on 08/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
